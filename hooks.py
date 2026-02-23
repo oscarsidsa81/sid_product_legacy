ead6d0616ce41393228bb7bfb2faab31a64fcdbb
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-23 16:11:48 +0100

    modificado para evitar duplicar campos

--- hooks.py
+++ hooks.py
@@ -5,7 +5,6 @@ def post_init_copy_legacy_to_base(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
 
     PT = env["product.template"].sudo()
-    PP = env["product.product"].sudo()
 
     # Template
     template_map = [
@@ -18,19 +17,6 @@ def post_init_copy_legacy_to_base(cr, registry):
     for src, dst in template_map:
         if src in PT._fields and dst in PT._fields:
             records = PT.search([(src, "!=", False)])
-            for rec in records:
-                if rec[src] and rec[dst] != rec[src]:
-                    rec.write({dst: rec[src]})
-
-    # Product variant
-    product_map = [
-        ("x_pasillo", "sid_pasillo"),
-        ("x_forecast_madrid", "sid_forecast_madrid"),
-    ]
-
-    for src, dst in product_map:
-        if src in PP._fields and dst in PP._fields:
-            records = PP.search([(src, "!=", False)])
             for rec in records:
                 if rec[src] and rec[dst] != rec[src]:
                     rec.write({dst: rec[src]})
